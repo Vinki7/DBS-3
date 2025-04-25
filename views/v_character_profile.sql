2449ef2f45383585cd885374c114d97d3fa74b54
Vinki7 <xvinklers@stuba.sk>
2025-04-25 08:45:24 +0200

    feat: character profile view adjusted

--- views/v_character_profile.sql
+++ views/v_character_profile.sql
@@ -1,13 +1,12 @@
 DROP VIEW IF EXISTS v_character_profile;
+-- This view provides a comprehensive profile of each character, including their attributes, inventory items, and learned spells.
+-- It aggregates data from multiple tables to present a complete picture of the character's status and capabilities.
 CREATE OR REPLACE VIEW v_character_profile AS
 SELECT 
     c.id AS character_id,
     c.state,
     cl.name AS class_name,
     c.experience_points,
-    ARRAY_AGG(DISTINCT a.name || ': ' || ca.base_value) AS attributes_base,
-    ARRAY_AGG(DISTINCT i.name) AS iventory_items,
-    ARRAY_AGG(DISTINCT s.name) AS learned_spells,
     f_attribute_value(c.id, (
         SELECT attr.id
         FROM "Attributes" AS attr
@@ -37,7 +36,10 @@ SELECT
         SELECT attr.id
         FROM "Attributes" AS attr
         WHERE attr.name = 'Constitution'
-    )) AS constitution_total
+    )) AS constitution_total,
+    ARRAY_AGG(DISTINCT a.name || ': ' || ca.base_value) AS attributes_base,
+    ARRAY_AGG(DISTINCT i.name) AS iventory_items,
+    ARRAY_AGG(DISTINCT s.name) AS learned_spells
 FROM "Characters" c
 JOIN "Classes" cl ON c.class_id = cl.id
 LEFT JOIN "CharacterAttributes" ca ON ca.character_id = c.id
@@ -48,5 +50,4 @@ LEFT JOIN "CharacterSpells" cs ON cs.character_id = c.id
 LEFT JOIN "Spells" s ON s.id = cs.spell_id
 GROUP BY c.id, c.state, cl.name, c.experience_points;
 
-SELECT * FROM v_character_profile
-WHERE character_id = 2; -- Example call to the view
+SELECT * FROM v_character_profile;
